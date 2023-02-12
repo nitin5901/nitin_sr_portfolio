d8aaec12cc53a362909b0827a5888912e6e94eb4
nitin5901 <nitinsingh5901@gmail.com>
2023-02-12 11:31:57 +0530

    Deployment Issues Fixing

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,5 @@
-import  Image  from 'next/image';
-import styles from './about.module.css'
+import  Image  from "next/image";
+import styles from "./about.module.css"
 import Button from "../../Components/Button/button";
 import Link from "next/link";
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css"
 import Button from "../Components/Button/button";
 import Image from "next/image";
 
