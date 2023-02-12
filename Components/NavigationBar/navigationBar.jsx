250a3def027f3a75bde8342a6a5a82e0c22a8218
nitin5901 <nitinsingh5901@gmail.com>
2023-02-12 11:08:24 +0530

    Navigation Bar Project Route changed

--- Components/NavigationBar/navigationBar.jsx
+++ Components/NavigationBar/navigationBar.jsx
@@ -9,7 +9,7 @@ const NavigationBar = function (){
             <ul className={styles.pageNavigation}>
                 <li> <Link href="/"> Home </Link> </li>
                 <li> <Link href="/about"> About </Link> </li>
-                <li> <Link href="/projects"> Projects </Link> </li>
+                <li> <Link href="/"> Projects </Link> </li>
             </ul>
             <Link href="/contact"> <Button title={"Contact Me"} /> </Link>
         </nav>
